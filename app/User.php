aaad570b2d923714c4b40b96be93eb2c00734c27
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2016-08-31 19:57:48 +0300

    Upgrade to Laravel 5.3

--- app/User.php
+++ app/User.php
@@ -2,44 +2,30 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends Authenticatable
 {
-    use Authenticatable, CanResetPassword;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
-
-    /**
-     * The attributes that should be casted to native types.
-     *
-     * @var array
-     */
-    protected $casts = ['id' => 'integer'];
-
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     /**
      * Return the author user attributes.
@@ -49,12 +35,12 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     public function getAuthor()
     {
         return [
-            'id'     => $this->id,
-            'name'   => $this->name,
-            'email'  => $this->email,
-            'url'    => $this->url,
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'url' => $this->url,
             'avatar' => 'gravatar',
-            'admin'  => $this->role === 'admin',
+            'admin' => $this->role === 'admin',
         ];
     }
 }
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('comments');
+});
+
+Route::get('basic', function () {
+    return view('comments-basic');
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
